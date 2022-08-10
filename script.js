94d52add6e2e990fd541564858615891532aadf6
vishnu5599 <108262604+vishnu5599@users.noreply.github.com>
2022-08-11 00:17:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function change(){
+    document.body.style.backgroundColor=
+    'rgb(' +Math.round(Math.random() * 255)+
+    ',' + Math.round(Math.random() * 255)+
+    ',' + Math.round(Math.random() * 255)+')';
+}
